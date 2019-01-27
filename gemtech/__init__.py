cf82b7933c6723c996072549fac71b86f10627aa
liucjray <liucjray@gmail.com>
2019-01-27 18:46:27 +0800

    gemtech cqssc

---
+++ gemtech/__init__.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from settings import env
---
+++ gemtech/cqssc.py
@@ -0,0 +1,31 @@
+import os
+from gemtech import *
+
+chrome_driver_path = os.getenv("CHROME_DRIVER_PATH")
+url = 'http://www.gem-ds.com/lottery.html?lottery=cqssc'
+
+browser = webdriver.Chrome(chrome_driver_path)
+delay = 1
+try:
+    browser.get(url)
+    WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'table-ball')))
+    wait = WebDriverWait(browser, delay)
+
+    issues = []
+    issuesElements = browser.find_elements_by_css_selector('.lottery-table td:nth-child(2)')
+    for element in issuesElements:
+        issues.append(element.get_attribute('innerHTML'))
+    print(issues)
+
+    codes = []
+    codesElements = browser.find_elements_by_css_selector('.lottery-table td:nth-child(3)')
+    for element in codesElements:
+        tmpCodes = element.find_elements_by_class_name('table-ball')
+        issueCodes = []
+        for tmpCode in tmpCodes:
+            issueCodes.append(tmpCode.get_attribute('innerHTML'))
+        codes.append(','.join(issueCodes))
+    print(codes)
+
+finally:
+    browser.close()
