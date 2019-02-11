a7c31a501633aeaf0efeadbbfd43729bf158e37f
ray <ray.kh.gy@gmail.com>
2019-02-11 14:33:40 +0800

    調整 ahfc 抓取方法放入 SourceAHFC

--- lib/sources/SourceAHFC.py
+++ lib/sources/SourceAHFC.py
@@ -1,3 +1,8 @@
+import os
+import time
+from settings import env
+from drawsources.ah_upload_cn.html import *
+
 import datetime
 import requests
 from addict import Dict
@@ -7,13 +12,51 @@ from lib.IssueInfo import *
 class SourceAHFC:
     def __init__(self, settings):
         self.settings = Dict(settings)
-        self.data = self.settings.to_dict().get('data')
+        self.data = []
         self.codes = []
         self.issues = []
 
     def clean(self):
+        self.data = self.get_data()
         self.codes = []
         self.issues = []
+
+    def get_data(self):
+        chrome_driver_path = os.getenv("CHROME_DRIVER_PATH")
+        url = 'http://ah.uplottery.cn/zst/ah_k3_pc/jb?key='
+        options = ChromeOptions()
+        options.headless = True
+        browser = webdriver.Chrome(chrome_driver_path, options=options)
+
+        try:
+            browser.get(url)
+
+            issues = []
+            issuesElements = browser.find_elements_by_css_selector('.term-scroller table:nth-child(1) tr td')
+            for element in issuesElements:
+                issues.append(element.get_attribute('innerHTML'))
+
+            # 移除無用資訊
+            issues = issues[0:-5]
+
+            # 休息一秒確保號碼全部出現
+            time.sleep(1)
+
+            codes = []
+            trs = browser.find_elements_by_css_selector('#base-table tbody:nth-child(1) tr')
+            for tr in trs:
+                issueCodes = []
+                tds = tr.find_elements_by_tag_name('td')
+                for td in tds[0:3]:
+                    code = td.find_element_by_tag_name('span').get_attribute('innerHTML')
+                    issueCodes.append(code)
+                codes.append(','.join(issueCodes))
+
+            result = dict(zip(issues, codes))
+            return result
+
+        finally:
+            browser.close()
 
     def get_codes(self):
         self.codes = list(self.data.values())
@@ -45,10 +88,6 @@ class SourceAHFC:
             print('Validate Error!')
 
     def validate(self):
-
-        # print(len(self.codes))
-        # print(len(self.issues))
-
         return len(self.codes) == len(self.issues) \
                and len(self.codes) > 0 \
                and len(self.issues) > 0
